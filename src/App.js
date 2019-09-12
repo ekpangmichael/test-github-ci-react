e5d437a7397e21279e1f11eb17249d797e7a1e72
Michael Ekpang <weezyval93@gmail.com>
2019-09-12 15:56:16 +0100

    update ci file, added support for depend on

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yes! this app was deployed using Github CI/CD
+          Yesss! this app was deployed using Github CI/CD
         </p>
         <a
           className="App-link"
